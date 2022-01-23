fd7829cc25a25a6035d6652af0fa6b8dd8648675
Oryange <oryangestrifezze@gmail.com>
2022-01-23 18:09:59 -0300

    Adiciona Controller

---
+++ MyTodo/Controllers/TodoController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTodo.Controllers
+{
+    [ApiController]
+    [Route(template:"v1")]
+    public class TodoController : ControllerBase
+    {
+        
+    }
+}
--- MyTodo/Migrations/20220123210536_AddDateTime.Designer.cs
+++ MyTodo/Migrations/20220123210536_AddDateTime.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyTodo.Data;
 
 namespace MyTodo.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220123210536_AddDateTime")]
+    partial class AddDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
