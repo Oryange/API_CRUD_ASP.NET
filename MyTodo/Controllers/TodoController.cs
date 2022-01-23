03883f8e1d693fdd2063d148dfc1f81bcb654982
Oryange <oryangestrifezze@gmail.com>
2022-01-23 18:26:48 -0300

    Configura conexao de banco de dados

--- MyTodo/Controllers/TodoController.cs
+++ MyTodo/Controllers/TodoController.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MyTodo.Models;
 
 namespace MyTodo.Controllers
 {
-    [ApiController]
-    [Route(template:"v1")]
-    public class TodoController : ControllerBase
+  [ApiController]
+  [Route(template: "v1")]
+  public class TodoController : ControllerBase
+  {
+    [HttpGet]
+    [Route(template:"todos")]
+    public List<Todo> Get()
     {
-        
+      return new List<Todo>();
     }
+  }
 }
--- MyTodo/Startup.cs
+++ MyTodo/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyTodo.Data;
 
 namespace MyTodo
 {
@@ -11,6 +12,7 @@ namespace MyTodo
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+      services.AddDbContext<AppDbContext>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
