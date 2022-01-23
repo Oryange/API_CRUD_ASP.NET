00f0967cb1a0864f75e83c2b4ff593524a225c8f
Oryange <oryangestrifezze@gmail.com>
2022-01-23 12:27:34 -0300

    Adiciona conexoes com o db

--- MyTodo/Data/AppDbContext.cs
+++ MyTodo/Data/AppDbContext.cs
@@ -1,10 +1,15 @@
-
+using Microsoft.EntityFrameworkCore;
+using MyTodo.Models;
 
 namespace MyTodo.Data
 {
-    //arquivo que faz o depara
-    public class AppDbContext
-    {
-        
-    }
+  // arquivo que faz o depara: classe 'x' vai ser tabela 'z' no banco
+  public class AppDbContext : DbContext
+  {
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlite(connectionString: "DataSource=app.db; Cache=Shared");
+  }
 }
