85f753b639b3bcc5d9d9e9e9fab309f4ff16efa8
Oryange <oryangestrifezze@gmail.com>
2022-01-23 18:37:23 -0300

    Metodo get de um item

--- MyTodo/Controllers/TodoController.cs
+++ MyTodo/Controllers/TodoController.cs
@@ -13,7 +13,7 @@ namespace MyTodo.Controllers
   {
     [HttpGet]
     [Route(template: "todos")]
-    public async Task<IActionResult> Get(
+    public async Task<IActionResult> GetAsync(
         [FromServices] AppDbContext context)
     {
       var todos = await context
@@ -21,6 +21,22 @@ namespace MyTodo.Controllers
       .AsNoTracking()
       .ToListAsync();
       return Ok(todos);
+    }
+
+    [HttpGet]
+    [Route(template: "todos/{id}")]
+    public async Task<IActionResult> GetByIdAsync(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id)
+    {
+      var todo = await context
+      .Todos
+      .AsNoTracking()
+      .FirstOrDefaultAsync(x => x.Id == id);
+
+      return todo == null
+      ? NotFound()
+      : Ok(todo);
     }
   }
 }
